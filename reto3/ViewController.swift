e7f4306e54d19d6b942be6a10ba9575eb38a11fb
margaret ancco <manccoca@emeal.nttdata.com>
2022-12-02 11:34:53 -0500

    Initial Commit

---
+++ reto3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reto3
+//
+//  Created by Margaret Alexia Ancco Calloapaza on 2/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
